939d72ba2cf1388b66f5067e53826b8c67ed373b
Mederic Bellemare <bellemaremederic@gmail.com>
2017-10-09 21:07:57 -0400

    toggle more info

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$('document').ready(function(){
+   console.log('Main.js loaded');
+    toggleEtude();
+    toggleStudy();
+   $('#btnMoreEtude').click(function(){
+       toggleEtude();
+   });
+    $('#btnMoreStudy').click(function(){
+        toggleStudy();
+    });
+
+
+   function toggleEtude(){
+       $('#moreEtude').toggle('hide');
+       $('#btnMoreEtude').toggleClass('fa-rotate-180');
+   }
+   function toggleStudy(){
+       $('#moreStudy').toggle('hide');
+       $('#btnMoreStudy').toggleClass('fa-rotate-180');
+   }
+});
