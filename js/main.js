ce60ad81a609fcfb5e21169f23434a4f20cbf7ae
Médéric Bellemare <bellemaremederic@gmail.com>
2020-02-10 13:22:50 -0500

    print mod

--- js/main.js
+++ js/main.js
@@ -5,26 +5,38 @@ function documentLoaded() {
     var btnEtude = document.getElementById("btnMoreEtude");
     var sectionMoreEtude = document.getElementById("moreEtude");
     var btnExp = document.getElementById("btnMoreExp");
-    var sectionMoreExp = document.getElementById("moreExp")
+    var sectionMoreExp = document.getElementById("moreExp");
+    var allMoreSection = document.getElementsByClassName("moreSection");
+    
+    console.log('%c Besoin d\'information \n bellemaremederic@gmail.com ', 'background: #001242; color: #2e86de; font-size: 3em;');
 
     init();
 
+    window.addEventListener("beforeprint",function(){
+        console.log("beforeprint")
+        for (const element of allMoreSection) {
+            element.style.height = element.dataset.maxHeight;
+            element.style.opacity = 1;
+            element.dataset.status = "open";
+        }
+    });
+
     function init() {
-        allMoreSection = document.getElementsByClassName("moreSection");
+        
         for (const element of allMoreSection) {
             element.dataset.maxHeight = element.offsetHeight;
-            element.style.height = "0px";
+            element.style.height = 0;
             element.style.opacity = 0;
             element.dataset.status = "close";
             var nextButton = element.nextElementSibling;
             nextButton.addEventListener("click", function(clickEvent) {
-                toggleSection(clickEvent, element)
+                toggleSection(clickEvent, element);
             });
         }
     }
 
     function toggleSection(event, section) {
-        event.target.classList.toggle("fa-rotate-180")
+        event.target.classList.toggle("fa-rotate-180");
         animation(section);
     }
 
@@ -35,7 +47,6 @@ function documentLoaded() {
             element.style.height = maxHeight;
             element.style.opacity = 1
             dataset.status = "open"
-
         } else {
             element.style.height = 0;
             element.style.opacity = 0;
