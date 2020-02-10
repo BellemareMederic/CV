1e03c22889731160b213933817d909f20b833f0a
Médéric Bellemare <bellemaremederic@gmail.com>
2020-02-09 22:56:13 -0500

    retrait de jquery et toggle a la main & font-awsome en non compiler sur le repo & utilisation de yarn

--- js/main.js
+++ js/main.js
@@ -1,24 +1,45 @@
-$('document').ready(function () {
-    console.log('Main.js loaded');
-    $('#btnMoreEtude').click(function () {
-        toggleEtude();
-    });
+document.addEventListener('DOMContentLoaded', documentLoaded, false);
 
-    $('#btnMoreExp').click(function () {
-        toggleExp();
-        $("html, body").animate({ scrollTop: $(document).height() }, "slow");
-        return false;
-    });
 
-    //$( window ).bind( "onbeforeprint", toggleEtude );
+function documentLoaded() {
+    var btnEtude = document.getElementById("btnMoreEtude");
+    var sectionMoreEtude = document.getElementById("moreEtude");
+    var btnExp = document.getElementById("btnMoreExp");
+    var sectionMoreExp = document.getElementById("moreExp")
 
-    function toggleEtude() {
-        $('#moreEtude').toggle('hide');
-        $('#btnMoreEtude').toggleClass('fa-rotate-180');
+    init();
+
+    function init() {
+        allMoreSection = document.getElementsByClassName("moreSection");
+        for (const element of allMoreSection) {
+            element.dataset.maxHeight = element.offsetHeight;
+            element.style.height = "0px";
+            element.style.opacity = 0;
+            element.dataset.status = "close";
+            var nextButton = element.nextElementSibling;
+            nextButton.addEventListener("click", function(clickEvent) {
+                toggleSection(clickEvent, element)
+            });
+        }
     }
 
-    function toggleExp() {
-        $('#moreExp').toggle('hide');
-        $('#btnMoreExp').toggleClass('fa-rotate-180');
+    function toggleSection(event, section) {
+        event.target.classList.toggle("fa-rotate-180")
+        animation(section);
     }
-});
+
+    function animation(element) {
+        var dataset = element.dataset;
+        var maxHeight = dataset.maxHeight;
+        if(dataset.status == "close") {
+            element.style.height = maxHeight;
+            element.style.opacity = 1
+            dataset.status = "open"
+
+        } else {
+            element.style.height = 0;
+            element.style.opacity = 0;
+            dataset.status = "close"
+        }
+    }
+}
