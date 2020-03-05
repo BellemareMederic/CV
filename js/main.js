3e574a9ba1a17f2a1a75a3a55cb26942e3d23a1f
Médéric Bellemare <bellemaremederic@gmail.com>
2020-03-04 20:39:44 -0500

    Retrait de lutilisation des dataset pour la hauteur des contenue caché

--- js/main.js
+++ js/main.js
@@ -2,10 +2,6 @@ document.addEventListener('DOMContentLoaded', documentLoaded, false);
 
 
 function documentLoaded() {
-    var btnEtude = document.getElementById("btnMoreEtude");
-    var sectionMoreEtude = document.getElementById("moreEtude");
-    var btnExp = document.getElementById("btnMoreExp");
-    var sectionMoreExp = document.getElementById("moreExp");
     var allMoreSection = document.getElementsByClassName("moreSection");
     
     console.log('%c Besoin d\'information \n bellemaremederic@gmail.com ', 'background: #001242; color: #2e86de; font-size: 3em;');
@@ -15,16 +11,14 @@ function documentLoaded() {
     window.addEventListener("beforeprint",function(){
         console.log("beforeprint")
         for (const element of allMoreSection) {
-            element.style.height = element.dataset.maxHeight;
+            element.style.height = element.scrollHeight;
             element.style.opacity = 1;
             element.dataset.status = "open";
         }
     });
 
     function init() {
-        
         for (const element of allMoreSection) {
-            element.dataset.maxHeight = element.offsetHeight;
             element.style.height = 0;
             element.style.opacity = 0;
             element.dataset.status = "close";
@@ -42,9 +36,8 @@ function documentLoaded() {
 
     function animation(element) {
         var dataset = element.dataset;
-        var maxHeight = dataset.maxHeight;
         if(dataset.status == "close") {
-            element.style.height = maxHeight;
+            element.style.height = element.scrollHeight;
             element.style.opacity = 1
             dataset.status = "open"
         } else {
